9308eecdd14b80bb7d1a83b747d628b19013d99b
almiche <chamich196@hotmail.com>
2018-02-20 21:25:27 -0500

    Calibration function for esp sensors

--- Esp_Components/alarm.ino
+++ Esp_Components/alarm.ino
@@ -1,31 +1,30 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 #define sensor A0
 
 const char* ssid     = "raffy1968";
 const char* password = "raffy2011";
-int sensitivity = 0;
+String sensitivity = "";
 int start = 0;
 const String HOST = "hotelroom-iot-mike.herokuapp.com";
-ESP8266WiFiMulti WiFiMulti;
 
 
 void setup ()
 {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("raffy1968", "raffy2011");
+  WiFi.begin("raffy1968", "raffy2011");
   pinMode (sensor, INPUT) ;
   initialCalibration();
 }
 
 void loop (){
   int currentMotion = analogRead(sensor);
-  if((WiFiMulti.run() == WL_CONNECTED)) {
+  if(WiFi.status() == WL_CONNECTED) {
     int offset = currentMotion - start;
-    Serial.prinln(currentMotion + " - " + start + " = " + offset);
-    //Temporarily in comments for testing purposes
+//    Serial.println("Current: " + currentMotion);
+//    Serial.println("Start: " + start );
+//    Serial.println("Offset: " + offset);
 //    if( offset > sensitivity){
 //          getRequest(HOST,"/api/v0.1/alarm/status","location=bedroom");
 //        }
@@ -37,26 +36,28 @@ void loop (){
 }
 
 String getRequest(String host,String url,String queryString){
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-    String url = url + "?" + queryString;
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-    
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" + 
-                 "Connection: close\r\n\r\n");
-    return client.println();
+    HTTPClient http;  //Declare an object of class HTTPClient
+    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+      http.begin("http://" + HOST + url + "?" + queryString);  //Specify request destination
+      int httpCode = http.GET();                                                                  //Send the request
+      String payload;
+      if (httpCode > 0) { //Check the returning code
+   
+        payload = http.getString();   //Get the request response payload
+        Serial.println(payload);                     //Print the response payload
+      }
+   
+      http.end();   //Close connection
+      return payload;
+    }
 }
 
 void initialCalibration(){
+  Serial.println("Calibrating in 5 seconds ......");
   delay(5000);
-  Serial.prinln("Calibrating in 5 seconds ......");
   int currentMotion = analogRead(sensor);
   start = currentMotion; 
-  sensitivty = getRequest(HOST,"/api/v1.0/alarm/status","").toInt();
+  Serial.println("Current motion is now : " + start );
+  sensitivity = getRequest(HOST,"/api/v1.0/configuration/sensitivity","");
+  Serial.println("Sensitivity is now : " + sensitivity );
 }
