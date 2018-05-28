0b980020282742325a9e9327e865a491ed24d584
almiche <chamich196@hotmail.com>
2018-05-27 20:46:49 -0400

    Documentation added and clearing of local data cache

--- cruncher.py
+++ cruncher.py
@@ -4,6 +4,7 @@ import serial
 import os
 import sqlite3
 import numpy as np
+import time
 
 def thresholding_algo(y, lag, threshold, influence):
     signals = np.zeros(len(y))
@@ -40,22 +41,42 @@ if __name__ == "__main__":
     influence = 1 # Influence of new point
     timeout = 1 # Read frequency on the serial connection
     ser = serial.Serial("/dev/cu.wchusbserial14510", 115200, timeout = 1)
+    spike_flag = False
+    nth_point = 0 # Tracks which collection point we're at since list db is being dumped every few minutes
     while True:
         read_out = 0
+
         try:
             read_out = int(ser.readline().decode('utf-8'))
         except Exception as e:
             print(type(e))
+
         db.append(read_out)
-        print("Point is currently %s and list is currently at %s" % (read_out,len(db)))
-        if len(db) > lag:
-            result = (thresholding_algo(db,lag,threshold,influence))['signals']
-            if(abs(result[-1]) == 1):
-                # dump last minute of data to server
-                print("Spike!")
+        nth_point += 1
+        print("Point is currently %s and list is currently at %s" % (read_out,nth_point))
+
+        if nth_point > lag: # Collect data points for the moving average filter lag
+            result = (thresholding_algo(db,lag,threshold,influence))
+            if(abs(result['signals'][-1]) == 1): # Spike detected
+                spike_flag = True
+                print("Motion detected!")
             pass
         else:
-            print("herer")
+            print("Calibrating %d seconds remaining ..." % (lag - nth_point))
             print(db)
-        if(len(db)%(60/timeout) == 1): # Dump data to mysqlite
-            print("Database is currently at %s a dump is necessary" % len(db))
+        if(nth_point%(60/timeout) == 1 and nth_point > 1): # Dump data to mysqlite
+            print("Database is currently at %s dumping" % len(db))
+            '''
+             Once a dump is being done, the last lag number of elements of the current list are moved to the front of the list 
+             and the rest of the values are cleaned out this is in order for the moving average filter to still have values to lag behind with
+             and compare against
+            '''
+            temp_lag_storage = db[-lag:]
+            if spike_flag:
+                spike_flag = False
+                result = {
+                    'spikes':result['signals'],
+                    'raw_signals':db
+                }
+            # TODO: Dump data here
+            db = temp_lag_storage
