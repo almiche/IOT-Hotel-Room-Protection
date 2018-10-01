8aba3416b3e1e24c7c08a0679301dcb9ffe3853e
almiche <chamich196@hotmail.com>
2018-09-30 20:08:57 -0400

    Create backend service with database

---
+++ data_portal/controller.py
@@ -0,0 +1,70 @@
+from threading import Thread
+import sys
+import os
+from registry import *
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO
+from werkzeug.security import generate_password_hash, \
+     check_password_hash
+from flask_hashing import Hashing
+import secrets
+
+class Server():
+
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.socketio = SocketIO(self.app)
+        self.hashing = Hashing(self.app)
+        self.app.config['SECRET_KEY'] = 'secret!'
+        self.register_routes()
+        self.app.run(host='0.0.0.0', port=5000,debug=True)
+
+    def register_routes(self):
+        @self.app.route('/')
+        def index():
+            return 'Welcome to the api',200
+
+        @self.app.route('/api/v1.0/users',methods=['GET','PUT'])
+        @self.app.route('/api/v1.0/users/<user>',methods=['GET'])
+        def handle_users(user=None):
+            if request.method == 'GET':
+                if user is not None:
+                    return jsonify(return_user(user))
+            if request.method == 'PUT':
+                password = request.json['password']
+                salt = secrets.token_hex(nbytes=16)
+                hash = self.hashing.hash_value(password, salt=salt)
+                create_new_user(username=username,salt=salt,hash=hash)
+                return 'New user has been added',200
+        
+        @self.app.route('/api/v1.0/users/<user>/device/<device>',methods=['GET','PUT'])
+        @self.app.route('/api/v1.0/users/<user>/device',methods=['GET'])
+        def handle_devices(user, device = None):
+            if request.method == 'GET':
+                return jsonify(return_devices_for_user(user,device))
+            if request.method == 'PUT':
+                if device:
+                    mac = device
+                    device_type = request.json['device_type']
+                    collector_version = request.json['collector_version']
+                    room = request.json['room']
+                    owner =request.json['owner']
+                    create_new_device(mac,device_type,collector_version,room,owner)
+                    # Add verification here
+                    return 'New device has been added',200
+
+        @self.app.route('/api/v1.0/users/<user>/device/<device>/Log/<log>',methods=['GET','PUT'])
+        @self.app.route('/api/v1.0/users/<user>/device/<device>/Log',methods=['GET'])
+        def handle_logs(user,device,log = None):
+            if request.method == 'GET':
+                return jsonify(return_logs_for_device(user,device,log))
+            if request.method == 'PUT':
+                if log:
+                    log_dump = request.json['log_dump']
+                    timestamp  = request.json['timestamp']
+                    device = request.json['device']
+                    create_new_log(log_dump,timestamp,device)
+                    return 'Log added',200
+
+if __name__ == "__main__":
+    Server()
---
+++ data_portal/registry.py
@@ -0,0 +1,98 @@
+from pony.orm import *
+import json
+import os
+import pry
+
+try:  
+   username = os.environ.get("DB_USER")
+   password = os.environ.get("DB_PASSWORD")
+except KeyError: 
+   print("Not exist environment value for %s" % "key_maybe_not_exist")
+
+db = Database()
+db.bind(provider='mysql', host='127.0.0.1',port=3306, user=username, passwd=password, db='IOT_HOTEL')
+
+#Create tables
+class User(db.Entity):
+    username = PrimaryKey(str,auto=False)
+    password_hash = Required(str)
+    salt =  Required(str)
+    devices = Set('Device')
+
+class Device(db.Entity):
+    mac = PrimaryKey(str,auto=False)
+    device_type = Required(str)
+    collector_version = Required(str)
+    room = Required(str)
+    owner = Required(User) 
+    logs = Set('Log')
+
+class Log(db.Entity):
+    device = Required(Device)
+    log_dump = Required(str)
+    timestamp = Required(str)
+
+#TODO OAuth
+@db_session
+def create_new_user(username,hash,salt):
+    users =  select(user for user in User if user.username == username)
+    if len(users) > 0:
+        print("Invalid Username exists already")
+    else:
+        user = User(username=username,salt=salt,password_hash=hash)
+        commit()
+        return 'User has been added',200
+
+@db_session
+def create_new_log(log_dump,timestamp,device):
+    log = Log(log_dump = log_dump,timestamp=timestamp,device=device)
+    commit()
+    return 'Log created',200
+
+@db_session
+def create_new_device(mac,device_type,collector_version,room,owner):
+    if Device[mac] is not None:
+        print("Device already registered")
+    else:
+        Device(mac=mac,device_type=device_type,collector_version=collector_version,room=room,owner=owner)   
+        return '',200
+
+@db_session
+def return_user(user):
+    return User[user].to_dict()
+
+@db_session
+def return_devices_for_user(owner_id,device_id=None):
+    if device_id is None:
+        device_list = []
+        devices = User[owner_id].devices
+        for device in devices:
+            device_list.append(device.to_dict())
+        return device_list
+    else:
+        device = Device[device_id] if Device[device_id].owner.username == owner_id else None
+        return device
+
+@db_session
+def return_logs_for_device(owner_id,device_id,log_id=None):
+    if log_id is None:
+        log_list = []
+        logs = select(log 
+                        for owner in User
+                        for device in owner.devices
+                        for log in device.logs 
+                        if (owner.username == owner_id and
+                            device.mac == device_id ))
+        for log in logs:
+            log_list.append(log.to_dict())
+        return log_list
+    else:
+        log = Log[log_id] if Log[log_id].device.mac == device_id and Log[log_id].device.owner.username == owner_id else None
+        return log.to_dict()
+
+db.generate_mapping(create_tables=True)
+
+
+create_new_user("mike","pass","hash")
+create_new_device("543f34fg34","Rasberry Pi","0.0.1","Kitchen","mike")
+create_new_log("34,346,346,32,135,2345,35","5 am","543f34fg34")
