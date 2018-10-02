b2e7ebba3b7a5d97cc972147795dd01b17ea6703
almiche <chamich196@hotmail.com>
2018-10-02 01:24:16 -0400

    Revamp pi app

--- collector/controller.py
+++ collector/controller.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.append("/home/mike/workspace/IOT-Hotel-Room-Protection/collector")
 from lib.cruncher import *
-from flask import Flask
+from flask import Flask, render_template
 import random
 import threading
 import requests
@@ -21,34 +21,40 @@ class Collector():
         self.name = random.getrandbits(128)
         self.app = Flask(__name__)
         self.register_routes()
+        try:  
+            self.serialport = os.environ.get("PORT")
+            self.owner = os.environ.get("OWNER")
+            self.mac = os.environ.get("MAC")
+        except KeyError: 
+            print("Not exist environment value for %s" % "key_maybe_not_exist")
         self.app.run(host='0.0.0.0', port=5000)
+        self.thread_name = random.getrandbits(128)
 
     def register_routes(self):
         @self.app.route('/config')
         def hello_world():
             return 'Welcome to the configuration page of IOT-Hotel-Room-Protection device {}'.format(self.name)
 
-        @self.app.route('/begin_collection/<thread_name>',methods = ['GET','POST'])
-        def collector(thread_name):
-            extractor = Thread(group=None, target=ingress, name=thread_name, args=(
+        @self.app.route('/begin_collection',methods = ['GET','POST'])
+        def collector():
+            self.extractor = Thread(group=None, target=ingress, name=self.thread_name, args=(
                 [],
                 50, # How far behind will the moving average lag, larger the better
                 3.5, # Number of standard deviations away from the moving average will cause a trigger
                 1, # Influence of new point
                 1, # Read frequency on the serial connection
-                serial.Serial(sys.argv[0], 115200, timeout = 1),
+                serial.Serial(self.serialport, 115200, timeout = 1),
                 False,
                 0,
-                self.name
+                self
             ), kwargs={})
-            collectors[thread_name] = extractor
-            extractor.start()
-            return 'Collection has started!'
+            self.extractor.start()
+            return 'Collection has started!',200
         
         @self.app.route('/stop_collection/<thread_name>',methods = ['GET','POST'])
         def kill(thread_name):
-            if thread_name in collectors.keys():
-                collectors[thread_name].join()
+                self.extractor.join()
+                return 'Collection has stopped',200
 
 
 if __name__ == "__main__":
